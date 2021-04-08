83a0efee9376c1db489dacbc19e64edd91e4a59e
Ryan47Liao <62736640+Ryan47Liao@users.noreply.github.com>
2021-04-09 07:20:13 +0800

    Testing

--- P1Q3/fa.py
+++ P1Q3/fa.py
@@ -2,10 +2,21 @@ import goody
 
 
 def read_fa(file : open) -> {str:{str:str}}:
+    """ 
+    returns the dictionary representing the finite automaton; 
+    hint: I used splicing and the zip function to build the inner dictionaries. (body is 6 lines).
+    INPUT: even;0;even;1;odd
+    OUTPUT: {'even' : {'0':'even','1':'odd'} }
+    """
     pass
 
 
 def fa_as_str(fa : {str:{str:str}}) -> str:
+    """
+    Returns a multi-line string (each line is ended by '\n'), 
+    which when printed shows the contents of the FA in the appropriate textual form:
+    sorted alphabetically by state, with a state's transitions sorted by their input value
+    """
     pass
 
     
